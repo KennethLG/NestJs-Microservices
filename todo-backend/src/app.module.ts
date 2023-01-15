4351b1f1f5fd7e88a64c4c4b85295fd4ed341a76
KennethLG <orsonleiziaga@gmail.com>
2023-01-15 11:09:34 -0500

    Remove unnecesary imports

--- todo-backend/src/app.module.ts
+++ todo-backend/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TodoController } from './todo/todo.controller';
-import { TodoService } from './todo/todo.service';
 import { TodoModule } from './todo/todo.module';
 import { ConfigModule } from '@nestjs/config';
 import config from 'config';
-import { HttpModule, HttpService } from '@nestjs/axios';
 
 @Module({
   imports: [
