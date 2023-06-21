496fb4a99f2a3a48dca2bedca7a1ee46f5c7538b
KennethLG <orsonleiziaga@gmail.com>
2023-06-20 20:49:08 -0500

    Rename files and add error handling to TodoService requests in components

--- todo-backend/src/components/todo/todo.service.ts
+++ todo-backend/src/components/todo/todo.service.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import config from 'config';
 import { lastValueFrom } from 'rxjs';
@@ -14,14 +14,22 @@ export class TodoService {
 
   async create(todo: Todo) {
     const endpoint = this.configService.endpoints.db.create;
-    const response = await lastValueFrom(this.httpService.post(endpoint, todo));
-    return response.data;
+    try {
+      const response = await lastValueFrom(this.httpService.post(endpoint, todo));
+      return response.data;
+    } catch (error) {
+      throw new HttpException(error.response.data, error.response.status);
+    }
   }
 
   async getAll() {
     const endpoint = this.configService.endpoints.db.getAll;
-    const response = await lastValueFrom(this.httpService.get(endpoint));
-    return response.data;
+    try {
+      const response = await lastValueFrom(this.httpService.get(endpoint));
+      return response.data;
+    } catch (error) {
+      throw new HttpException(error.response.data, error.response.status);
+    }
   }
 
   getTodoById(id: string) {
