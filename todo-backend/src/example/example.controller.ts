5201e5453abe483311ec02168d1500366def003c
KennethLG <orsonleiziaga@gmail.com>
2023-06-20 19:53:57 -0500

    Add example GET endpoint to ExampleController for logging and returning four items from ExampleService

--- todo-backend/src/example/example.controller.ts
+++ todo-backend/src/example/example.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ExampleService } from './example.service';
+import { take } from 'rxjs';
 
 @Controller('example')
-export class ExampleController {}
+export class ExampleController {
+  constructor(
+    private readonly exampleService: ExampleService
+  ){}
+
+  @Get('/')
+  getExample() {
+    console.log("running example")
+    const numbers = this.exampleService.getHello();
+    const takeFourNumbers = numbers.pipe(take(4));
+
+    takeFourNumbers.subscribe((x) => console.log('Next: ', x));
+  }
+}
