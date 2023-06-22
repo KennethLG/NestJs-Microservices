2bb2f8754210f9451987afff8e2e5d85ae72354d
KennethLG <orsonleiziaga@gmail.com>
2023-06-22 16:35:01 -0500

    Update default port number to 3001 in AppConfig

--- todo-db/config/index.ts
+++ todo-db/config/index.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('AppConfig', () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: parseInt(process.env.PORT, 10) || 3001,
   db: {
     url: process.env.DATABASE_URL || ''
   }
