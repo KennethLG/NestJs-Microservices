55f607384f3427c21c28e80d456b1dc1e782c9b6
KennethLG <orsonleiziaga@gmail.com>
2023-06-23 16:03:18 -0500

    Add AuthModule and related filesThis commit adds the AuthModule, AuthController, and AuthService files to the project. The AuthController and AuthService files are also tested using Jest

--- todo-backend/src/app.module.ts
+++ todo-backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TodoModule } from './components/todo/todo.module';
 import { ConfigModule } from '@nestjs/config';
 import { ExampleModule } from './components/example/example.module';
+import { AuthModule } from './auth/auth.module';
 import config from 'config';
 
 @Module({
@@ -12,6 +13,7 @@ import config from 'config';
     }),
     TodoModule,
     ExampleModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
