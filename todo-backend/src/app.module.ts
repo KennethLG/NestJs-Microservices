b83ecd9bb670e995ecc0200a79c549d246db2f4f
KennethLG <orsonleiziaga@gmail.com>
2023-06-26 09:05:09 -0500

    Refactor app.module.ts to import AuthModule from the correct path in components

--- todo-backend/src/app.module.ts
+++ todo-backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TodoModule } from './components/todo/todo.module';
 import { ConfigModule } from '@nestjs/config';
 import { ExampleModule } from './components/example/example.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './components/auth/auth.module';
 import config from 'config';
 
 @Module({
