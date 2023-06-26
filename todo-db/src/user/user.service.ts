6bd69a124c23be177fbc51e75a1a50153b783243
KennethLG <orsonleiziaga@gmail.com>
2023-06-26 08:57:35 -0500

    Create User schema and update UserModule to include UserService provider and export it. Also, modify UserService to use Mongoose Model to get a user by their ID

--- todo-db/src/user/user.service.ts
+++ todo-db/src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/schemas/user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async findOne(id: string): Promise<User | undefined> {
+    return this.userModel.findById(id).exec();
+  }
+}
